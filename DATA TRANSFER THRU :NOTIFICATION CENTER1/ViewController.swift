e90a99da86a3afc5ad787a67fe81dafedac96f0e
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-11-23 11:52:03 +0530

    Initial Commit

---
+++ DATA TRANSFER THRU :NOTIFICATION CENTER1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DATA TRANSFER THRU :NOTIFICATION CENTER1
+//
+//  Created by Syed.Reshma Ruksana on 23/11/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
